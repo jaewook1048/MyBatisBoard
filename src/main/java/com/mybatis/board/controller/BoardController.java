a73c99bfe28af71def5d388eef9ff9629277645c
kosil1048 <kosil1048@gmail.com>
2024-08-06 16:51:13 +0900

    게시글 작성 화면 구현

--- src/main/java/com/mybatis/board/controller/BoardController.java
+++ src/main/java/com/mybatis/board/controller/BoardController.java
@@ -3,9 +3,15 @@ package com.mybatis.board.controller;
 import com.mybatis.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @RequiredArgsConstructor // final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping("/save")
+    public String save() {
+        return "save";
+    }
 }
